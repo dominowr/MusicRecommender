aa789c0cc39fad5e2e244af57cdc1c193a26332e
Dominik <nik.wrzesien@gmail.com>
2024-03-19 02:08:13 +0100

    view for spotify connection has been updated

--- main_web_service/front/views/__init__.py
+++ main_web_service/front/views/__init__.py
@@ -1,1 +1,2 @@
 from .index.index_view import IndexView
+from .spotify_connection.spotify_connection_view import SpotifyConnection
--- main_web_service/front/views/spotify_connection/spotify_connection_view.py
+++ main_web_service/front/views/spotify_connection/spotify_connection_view.py
@@ -2,9 +2,9 @@ from django.views.generic import TemplateView
 
 
 class SpotifyConnection(TemplateView):
-    template_name = ''
+    template_name = 'spotify_connection/spotify_connection.html'
 
     def get(self, request, *args, **kwargs):
-        if request.method == 'POST':
+        if request.method == 'GET':
             pass
         return super().get(request, *args, **kwargs)
