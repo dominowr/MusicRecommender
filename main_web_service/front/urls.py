c4d67eb867a165dcc55b90772019af02f7899a99
Dominik <nik.wrzesien@gmail.com>
2024-03-19 02:06:40 +0100

    path for spotify connection has been added

--- main_web_service/front/urls.py
+++ main_web_service/front/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, SpotifyConnection
 
 app_name = 'front'
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('spotify-connection', SpotifyConnection.as_view(), name='connection')
 ]
