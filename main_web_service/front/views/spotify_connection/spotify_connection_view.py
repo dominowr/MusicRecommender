1ffbac9508a930801a3cf9e0e46f0ddb0d363bf7
dominowr <nik.wrzesien@gmail.com>
2024-03-21 20:55:00 +0100

    spotify connection view has been protected with login_required decorator

--- main_web_service/front/views/spotify_connection/spotify_connection_view.py
+++ main_web_service/front/views/spotify_connection/spotify_connection_view.py
@@ -1,6 +1,9 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
+@method_decorator(login_required, name='dispatch')
 class SpotifyConnection(TemplateView):
     template_name = 'spotify_connection/spotify_connection.html'
 
