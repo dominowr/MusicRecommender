1472755b132cf56bd1accc3eccb111cfa7d92934
Dominik <nik.wrzesien@gmail.com>
2024-03-28 03:29:30 +0100

    code restructuring, main_web_services will be replaced

--- main_web_service/front/views/spotify_connection/spotify_connection_view.py
+++ main_web_service/front/views/spotify_connection/spotify_connection_view.py
@@ -1,10 +1,20 @@
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
+import json
 
 
 class SpotifyConnection(TemplateView):
     template_name = 'spotify_connection/spotify_connection.html'
 
-    def get(self, request, *args, **kwargs):
-        if request.method == 'GET':
-            pass
-        return super().get(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        url = 'http://127.0.0.1:5000/api/hello/'
+        return redirect(url)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context = {
+            'session': self.request.session.get('user'),
+            'pretty': json.dumps(self.request.session.get('user'), indent=4)
+        }
+        return context
+
