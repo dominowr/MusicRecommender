083eac53f48940bd5fd719e33f8479542746898e
Dominik <nik.wrzesien@gmail.com>
2024-04-03 04:03:52 +0200

    path for user account added

--- client/frontend/urls.py
+++ client/frontend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, UserDashboardView
 
 app_name = 'frontend'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('account', UserDashboardView.as_view(), name='account'),
 ]
